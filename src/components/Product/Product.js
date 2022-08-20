75182e8de647ebdbd607b4783cfe34f2857f7194
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2022-08-21 00:31:04 +0600

    Feature is fecthed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const {img, name, seller, price, stock} = props.product;
+    const { img, name, seller, price, stock, features } = props.product;
+    console.log(props.product)
     return (
         <div className="product">
             <div className='product-image'>
@@ -9,11 +10,21 @@ const Product = (props) => {
             </div>
 
             <div className='product-name'>
-               <h4>{name}</h4>
-               <br />
-               <p className='info'><small>By: {seller}</small></p>
-               <p className='info'><small>Price: {price}</small></p>
-               <p className='info'><small>Only {stock} left in the stock - Order soon</small></p>
+              <div>
+                    <h4>{name}</h4>
+                    <br />
+                    <p className='info'><small>By: {seller}</small></p>
+                    <p className='info'><small>Price: {price}</small></p>
+                    <p className='info'><small>Only {stock} left in the stock - Order soon</small></p>
+              </div>
+              <div className='specs'>
+                <h4>Features</h4>
+                    <ul>
+                        {
+                            features.map(f => <li>{f.description}: <strong>{f.value}</strong></li>)
+                        }
+                    </ul>
+              </div>
             </div>
         </div>
     );
