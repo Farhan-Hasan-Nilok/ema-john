e7b394bf27e063507d3bc3e85963d219141f399a
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2022-08-25 01:25:27 +0600

    minor style changes

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 import './Product.css';
 const Product = (props) => {
@@ -16,12 +17,13 @@ const Product = (props) => {
               <div>
                     <h4>{name}</h4>
                     <br />
+                    {/* <p className='rating'><small><FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStar} /></small></p> */}
                     <p className='info'><small>By: {seller}</small></p>
                     <p className='info'><small>Price: ${price}</small></p>
                     <p className='info'><small>Only {stock} left in the stock - Order soon</small></p>
                     <button className='main-button' onClick={() => props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
               </div>
-              <div className='specs'>
+              <div className='specs'>  
                 <h4>Features</h4>
                     <ul>
                         {
